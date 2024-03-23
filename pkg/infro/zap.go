18d8772687c46fa5842592aba7c6e39c642ae7ff
colethienes <cole.thienes@gmail.com>
2024-03-23 17:52:16 +0000

    feat: export logger context

---
+++ pkg/infro/zap.go
@@ -0,0 +1,14 @@
+package infro
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/infro-io/infro-core/internal/xzap"
+)
+
+// NewZapContext sets the logger that Infro will use.
+func NewZapContext(ctx context.Context, log *zap.Logger) {
+	xzap.NewContext(ctx, log)
+}
